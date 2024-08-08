bb9e767a046e6f38e3cbd4971f5fc8c45796d883
Prince Pal <palprince3690@gmail.com>
2024-08-08 19:42:28 +0530

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 
-
+import { useDispatch } from 'react-redux'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import authService from './appwrite/auth.js'
+import {login, logout} from './store/authSlice'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+const [loading, setLoading] = useState(true);
+const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Blog App</h1>
-    </>
-  )
+useEffect(() => {
+  authService
+  .getCurrentUser()
+  .then((userData) => {
+    console.log("User data: ", userData)
+    if(userData) dispatch(login({userData}))
+    else dispatch(logout())
+    
+  })
+  .finally(() => setLoading(false))
+
+},[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO:  <Outlet /> 
+        </main>
+
+        <Footer/>
+        
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
